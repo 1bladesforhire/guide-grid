decd6867616e6d06c5c956f40d2cd279457f4b3c
Rachel Blades (Vega Consulting LLC) <v-rablad@microsoft.com>
2018-02-06 10:48:21 -0800

    loading active section

--- main.js
+++ main.js
@@ -19,9 +19,38 @@ $(document).ready(function(){
 
 //smooth out the scroll to section, not sure about this one.. too long of a page
 $(document).on('click', 'a[href^="#"]', function (event) {
-  event.preventDefault();
+  
 
   $('html, body').animate({
       scrollTop: $($.attr(this, 'href')).offset().top
   }, 2000);
+  
+});
+
+$(window).on('load', function(){
+
+  //check for alternate link location
+  var locationName = location.href.split("#");
+
+  //set active link and scroll to location if not homepage
+  if(locationName.length>1){
+    var fileName = locationName.slice(-1);
+    var startingActive = $('a[href*="'+fileName+'"]');
+    console.log(locationName.length);
+
+    //set active link for the 
+    $('nav a').removeClass('active');
+    startingActive.addClass('active');
+    if(startingActive.parent().hasClass('sub-nav__item')){
+      $topLi = startingActive.parent().parent().parent();
+    } else {
+      $topLi = startingActive.parent();
+    }
+    $('nav li').removeClass('active');
+    $topLi.addClass('active');
+
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 2000);
+  }
 });
